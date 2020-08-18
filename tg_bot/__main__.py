44579651dc03733dfc38f580948f1fc19be2decf
GodTyson <69841706+GodTyson@users.noreply.github.com>
2020-08-18 13:27:02 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -43,14 +43,12 @@ I am an Anime themed group management bot with some fun extras [;)](https://tele
 
 You can find the list of available commands with /help.
 
-[Kigyō's Repo](https://github.com/Dank-del/EnterpriseALRobot) 
-
-The support group chat is at @YorktownEagleUnion
+The support group chat is at @AnnieSupportChat
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a part of *Eagle Union*
+I'm a part of * @Escrow4You *
 Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
